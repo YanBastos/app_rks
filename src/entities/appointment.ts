42caea2e7ae434ce93f6dc028ad1e130b858c998
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-06 20:48:49 -0300

    ...

---
+++ src/entities/appointment.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+export interface AppointmentProps {
+  customer: string
+  startAt: Date
+  endsAt: Date
+}
+
+export class Appointment {
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly
+  private props: AppointmentProps
+
+  get customer () {
+    return this.props.customer
+  }
+
+  get startAt () {
+    return this.props.startAt
+  }
+
+  get endsAt () {
+    return this.props.endsAt
+  }
+}
