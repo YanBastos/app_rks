bbfbc11b34848f03f38525d1097d36fd14e504ca
YanBastos <yanmoraesbastos@hotmail.com>
2024-02-09 15:30:49 -0300

    ...

--- src/use-cases/create-appointmennt.ts
+++ src/use-cases/create-appointmennt.ts
@@ -12,13 +12,10 @@ interface CreateAppointmentReq {
 type CreateAppointmentRes = Appointment
 
 export class CreateAppointment {
-    static execute(arg0: { customer: string; startsAt: Date; endsAt: Date }): any {
-        throw new Error("Method not implemented.")
-    }
     async execute({
         customer,
         startsAt,
-        endsAt
+        endsAt,
     }: CreateAppointmentReq): Promise<CreateAppointmentRes> {
         const appointment = new Appointment({
             customer,
